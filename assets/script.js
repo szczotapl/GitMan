adee1d3fda4730c3b684bd1efefeca577ca20882
Riviox <100956266+riviox@users.noreply.github.com>
2024-03-22 23:49:00 +0100

    added website

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,21 @@
 fetch('https://raw.githubusercontent.com/riviox/GitMan/main/packages.json')
     .then(response => response.json())
     .then(data => {
-    const packagesList = document.getElementById('packages-list');
-    data.forEach(package => {
-        const packageItem = document.createElement('div');
-        const packageName = document.createElement('a');
-        packageName.textContent = package.name;
-        packageName.href = package.repository;
-        packageName.target = "_blank";
-        packageItem.appendChild(packageName);
-        packagesList.appendChild(packageItem);
-    });
+        const packagesList = document.getElementById('packages-list');
+        let column;
+        data.forEach((package, index) => {
+            if (index % 5 === 0) {
+                column = document.createElement('div');
+                packagesList.appendChild(column);
+            }
+
+            const packageItem = document.createElement('div');
+            const packageName = document.createElement('a');
+            packageName.textContent = package.name;
+            packageName.href = package.repository;
+            packageName.target = "_blank";
+            packageItem.appendChild(packageName);
+            column.appendChild(packageItem);
+        });
     })
-.catch(error => console.error('Błąd podczas pobierania danych:', error));
+    .catch(error => console.error('Error:', error));
